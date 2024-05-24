d5e2e7a7195292253ebaff8bdf45fb4b28eafe9a
hql <2454618038@qq.com>
2024-05-24 21:08:45 +0800

    新增阅读功能

--- auto_print_click_chrome.py
+++ auto_print_click_chrome.py
@@ -1,14 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service  
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 import time
 import os
 import datetime
 
-# 创建一个 Chrome WebDriver 实例
-options = Options()
-# options.add_argument("--headless")  # 启用无头模式
 def read_config(file_path):
     # 读取config.txt文件并解析内容
     with open(file_path, 'r') as file:
@@ -27,64 +24,89 @@ def read_config(file_path):
 
     return config
 
-
-def file_rename(config , Type):
+def file_rename_word(config, Type):
     current_date = datetime.datetime.now().strftime("%Y%m%d")
-# 获取name参数
+    # 获取name参数
     name = config.get('name', '')
 
-# 查找当前目录练习开头的.pdf文件
+    # 查找当前目录练习开头的.pdf文件
     for file_name in os.listdir('.'):
         if file_name.startswith('练习') and file_name.endswith('.pdf'):
-        # 构造新的文件名
+            # 构造新的文件名
             new_file_name = f"{current_date} {name}{Type}.pdf"
-        # 重命名文件
+            # 重命名文件
             os.rename(os.path.join('.', file_name), os.path.join('.', new_file_name))
             print(f"文件 {file_name} 已重命名为 {new_file_name}")
             break  # 如果只想处理一个文件，可以加上break来提前结束循环
 
-config = read_config('./config.txt')
-# 获取参数
-name = config.get('name', '')
-Type = config.get('type', '')
-net = config.get('net', '')
+def file_rename_article(config):
+    current_date = datetime.datetime.now().strftime("%Y%m%d")
+    # 获取name参数
+    name = config.get('name', '')
+    # 查找当前目录练习开头的.pdf文件
+    for file_name in os.listdir('.'):
+        if file_name.startswith('练习') and file_name.endswith('.pdf'):
+            # 构造新的文件名
+            new_file_name = f"{current_date} {name}阅读.pdf"
+            # 重命名文件
+            os.rename(os.path.join('.', file_name), os.path.join('.', new_file_name))
+            print(f"文件 {file_name} 已重命名为 {new_file_name}")
+            break  # 如果只想处理一个文件，可以加上break来提前结束循环
 
-prefs = {
-    "download.prompt_for_download": False,
-    "savefile.default_directory": "/home/hql/git/my_git/csl_auto_print",  # 请替换为你的下载目录
-    "download.directory_upgrade": True,
-    "download.extensions_to_open": "pdf",
-}
-options.add_experimental_option("prefs", prefs)
+def main():
+    config = read_config('./config.txt')
+    # 获取参数
+    Type = config.get('type', '')
+    net = config.get('net', '')
 
-# 使用 --kiosk-printing 来启用 "silent printing"
-options.add_argument("--kiosk-printing")
+    prefs = {
+        "download.prompt_for_download": False,
+        "savefile.default_directory": "/home/hql/git/my_git/csl_auto_print",  # 请替换为你的下载目录
+        "download.directory_upgrade": True,
+        "download.extensions_to_open": "pdf",
+    }
+    options = Options()
+    options.add_experimental_option("prefs", prefs)
 
-# 请确保 chrome_driver_path 指向正确的 ChromeDriver 位置
-chrome_driver_path = "./chromedriver"  # 请替换为你的 ChromeDriver 路径
-service = Service(chrome_driver_path)
-driver = webdriver.Chrome(service=service, options=options)
+    # 使用 --kiosk-printing 来启用 "silent printing"
+    options.add_argument("--kiosk-printing")
 
-# 打开指定的 URL
-driver.get(net)
+    # 请确保 chrome_driver_path 指向正确的 ChromeDriver 位置
+    chrome_driver_path = "./chromedriver"  # 请替换为你的 ChromeDriver 路径
+    service = Service(chrome_driver_path)
+    driver = webdriver.Chrome(service=service, options=options)
 
-# 等待页面加载
-time.sleep(5)  # 根据实际情况调整等待时间
+    # 打开指定的 URL
+    driver.get(net)
 
-# 定义包含查找关键字的列表
-search_texts = ["打印中英文", "打印中文", "打印英文", "打印音标"]
-texts = ["中英", "中文", "英文", "音标"]
+    # 等待页面加载
+    time.sleep(5)  # 根据实际情况调整等待时间
 
-for text, Type in zip(search_texts, texts):
-    try:
-        # 动态构建XPath表达式并查找元素
-        element = driver.find_element(By.XPATH, f"//button[contains(text(), '{text}')]")
-        element.click()
-        # 在这里可以对找到的元素执行操作，比如点击
-        driver.execute_script("window.print();")
-        file_rename(config, Type)
+    # 定义包含查找关键字的列表
+    search_texts = ["打印中英文", "打印中文", "打印英文", "打印音标"]
+    texts = ["中英", "中文", "英文", "音标"]
 
-    except Exception as e:
-        print(f"未找到元素：'{text}', 错误信息：{e}")
+    if isinstance(Type, str):
+        if Type == "阅读":
+            print("正在打印阅读，请稍后...")
+            driver.execute_script("window.print();")
+            file_rename_article(config)
+        elif Type == "单词":
+            print("正在打印单词，请稍后...")
+            for text, T in zip(search_texts, texts):
+                try:
+                    # 动态构建XPath表达式并查找元素
+                    element = driver.find_element(By.XPATH, f"//button[contains(text(), '{text}')]")
+                    element.click()
+                    # 在这里可以对找到的元素执行操作，比如点击
+                    driver.execute_script("window.print();")
+                    file_rename_word(config, T)
+                except Exception as e:
+                    print(f"未找到元素：'{text}', 错误信息：{e}")
+        else:
+            print("类型type错误, 请检查config.txt文件")
 
-driver.quit()
+    driver.quit()
+
+if __name__ == "__main__":
+    main()

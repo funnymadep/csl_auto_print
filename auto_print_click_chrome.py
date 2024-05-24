c29b547e446a543d410aeeb1b6e1eb6c37cd4005
hql <2454618038@qq.com>
2024-05-24 16:30:48 +0800

    目前能跳过打印界面并且能打印出ppt

--- auto_print_click_chrome.py
+++ auto_print_click_chrome.py
@@ -7,8 +7,20 @@ import time
 options = Options()
 # options.add_argument("--headless")  # 启用无头模式
 
+# 设置 Chrome 选项
+prefs = {
+    "download.prompt_for_download": False,
+    "savefile.default_directory": "/home/hql/git/my_git/csl_auto_print",  # 请替换为你的下载目录
+    "download.directory_upgrade": True,
+    "download.extensions_to_open": "pdf",
+}
+options.add_experimental_option("prefs", prefs)
+
+# 使用 --kiosk-printing 来启用 "silent printing"
+options.add_argument("--kiosk-printing")
+
 # 请确保 chrome_driver_path 指向正确的 ChromeDriver 位置
-chrome_driver_path = "/home/hql/git/my_git/csl_auto_print/chromedriver"  # 请替换为你的 ChromeDriver 路径
+chrome_driver_path = "./chromedriver"  # 请替换为你的 ChromeDriver 路径
 service = Service(chrome_driver_path)
 driver = webdriver.Chrome(service=service, options=options)
 
@@ -19,14 +31,6 @@ driver.get(url)
 # 等待页面加载
 time.sleep(5)  # 根据实际情况调整等待时间
 
-# buttons = ["中英文", "英文", "中文", "音标"]  # 请根据实际的按钮文本进行调整
-# for button_text in buttons:
-#     button = driver.find_element_by_link_text(button_text)
-#     button.click()
-#     time.sleep(2)  # 等待页面响应
 driver.execute_script("window.print();")  # 执行 window.print() 函数
-#     time.sleep(2)  # 等待打印对话框打开
 
-
-# 关闭 WebDriver 实例
-# driver.quit()
+driver.quit()
